548738f5b3129b9f0a22f0c7889400e827c1b68e
anomit ghosh <anomit.ghosh@gmail.com>
2020-02-26 16:59:01 +0530

    fb user name corrected

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 var contract = '0x842a0a7f32325b4f965b6a18f64453348d5be98f';
 var maxPosts = 5;
 var twitter = 'anomit';
-var facebook = 'anomit';
+var facebook = 'anomit.ghosh';
 var github = 'anomit';
 var prefix = 'https://beta-api.ethvigil.com/contract/' + contract;
 var blockexplorer = 'https://goerli.etherscan.io';
